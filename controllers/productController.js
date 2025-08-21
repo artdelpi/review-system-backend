197d60e16402ab2aef6ff890f70093c159397c45
artdelpi <artdelpi@gmail.com>
2025-08-20 22:59:39 -0300

    feat: validate MongoDB connectivity in getProducts using find()

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,33 +1,63 @@
-const getProducts = (req, res) => {
-    res.json({ msg: "/api/products endpoint reached successfully" })
-};
+const Product = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
+
+//@acess public
+const getProducts = asyncHandler(async(req, res) => {
+    const products = await Product.find()
+    res.status(200).json({ msg: products });
+});
 
 
+//@acess public
 const getProduct = (req, res) => {
     res.json({ msg: "/api/products/:id endpoint reached successfully" })
 };
 
 
+//@acess public
 const getProductReviews = (req, res) => {
     res.json({ msg: "/api/products/:id/reviews endpoint reached successfully" })
 };
 
 
+//@route POST /api/products
+//@access public
 const createProduct= (req, res) => {
+    // Grabs product attributes
+    const {
+        name, 
+        category, 
+        price, 
+        description, 
+        image
+    } = req.body;
+
+    // Checks for provided attr correctness
+    if (!name || !category || !price || 
+        !description || !image) {
+            res.status(400); // Client Error: (400) Bad Request
+            const err = new Error();
+            next(err); // calls errorHandler middleware
+        };
+    
+    
     res.json({ msg: "/api/products endpoint reached successfully" });
 };
 
 
+//@access public
 const createProductReview = (req, res) => {
     res.json({ msg: "/api/products/:id/reviews endpoint reached successfully" })
 };
 
 
+//@access public
 const updateProduct = (req, res) => {
     res.json({ msg: "/api/products/:id endpoint reached successfully"});
 };
 
 
+//@access public
 const deleteProduct =  (req, res) => {
     res.json({ msg: "/api/products/:id endpoint reached successfully"});
 };
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 // Web framework; Application obj as "app" (server instance)
 const express = require("express");
-
-// 
-const connectDb = require("./config/dbConnection");
-
 // Reads the .env file and updates process.env
 const dotenv = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
 connectDb();
 
@@ -16,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 // Parses received JSONs
 app.use(express.json());
+
 // (any) HTTP requests involving users
 app.use("/api/users", require("./routes/userRoutes"));
 // (any) HTTP requests involving products
