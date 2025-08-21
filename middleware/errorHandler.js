97cbb63c6f356469aa0144c986cfd3149309160c
artdelpi <artdelpi@gmail.com>
2025-08-20 21:09:55 -0300

    fix: removes redundant JSON attribute

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -6,40 +6,34 @@ const errorHandler = (err,req,res,next) => {
         /* ====== Client Error (4xx) ====== */
         case 400:
             res.json({
-                error: "400 Bad Request",   
-                msg: err.message,
+                msg: "400 Bad Request",
                 stackTrace: err.stack
             });
         case 401:
             res.json({
-                error: "401 Unauthorized",   
-                msg: err.message,
+                msg: "401 Unauthorized",   
                 stackTrace: err.stack
             });
         case 403:
             res.json({
-                error: "403 Forbidden",   
-                msg: err.message,
+                msg: "403 Forbidden",   
                 stackTrace: err.stack
             });
         case 404:
             res.json({
-                error: "404 Not Found",   
-                msg: err.message,
+                msg: "404 Not Found",   
                 stackTrace: err.stack
             });
 
         /* ====== Server Error (5xx) ====== */
         case 500:
             res.json({
-                error: "500 Internal Server Error",   
-                msg: err.message,
+                msg: "500 Internal Server Error",   
                 stackTrace: err.stack
             });
         case 503:
             res.json({
-                error: "503 Service Unavailable",   
-                msg: err.message,
+                msg: "503 Service Unavailable",   
                 stackTrace: err.stack
             });
         default:
